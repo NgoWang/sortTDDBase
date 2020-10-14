18e74669021c1bf4449ca68b7c566b58a731035c
NgoWang <61744851+NgoWang@users.noreply.github.com>
2020-10-14 15:46:56 -0400

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -4,4 +4,25 @@
     Sorting functions
 */
 
-#include "sort.hpp"
+#include "sort.hpp"2
+namespace
+{
+    void swap(int &n1, int &n2)
+    {
+        int t = n1;
+        n1 = n2;
+        n2 = t;
+    }
+    
+    void order(int &n1, int &n2)
+    {
+        if(n1 > n2)
+            swap(n1, n2);
+    }
+}
+void sort(std::vector<int> &v)
+{
+    if(v.size() < 2)
+        return;
+    order(v[0], v[1]);
+}
